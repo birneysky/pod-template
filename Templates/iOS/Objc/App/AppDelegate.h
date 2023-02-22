564a3f26db1d461927c390e5420c6d9ac07a8049
Freddy <afreddyshere@gmail.com>
2023-02-22 21:08:09 +0800

    project configuration and xcode templates files for app

---
+++ Templates/iOS/Objc/App/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Project
+//
+//  Created by Bruce on 2023/2/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Templates/iOS/Objc/App/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Project
+//
+//  Created by Bruce on 2023/2/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
